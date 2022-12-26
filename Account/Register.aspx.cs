eda998e66d2154269adae278c220bc19cb45f0f2
hai <hahai1998@gmail.com>
2022-12-26 20:22:20 +0700

    initial

---
+++ Account/Register.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using WebApplication7.Models;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace WebApplication7.Account
+{
+    public partial class Register : Page
+    {
+        private string HashPassword(string password, byte[] salt = null, bool needsOnlyHash = false)
+        {
+            if (salt == null || salt.Length != 16)
+            {
+                // generate a 128-bit salt using a secure PRNG
+                salt = new byte[128 / 8];
+                using (var rng = RandomNumberGenerator.Create())
+                {
+                    rng.GetBytes(salt);
+                }
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            if (needsOnlyHash) return hashed;
+            // password will be concatenated with salt using ':'
+            return $"{hashed}:{Convert.ToBase64String(salt)}";
+        }
+        protected void CreateUser_Click(object sender, EventArgs e)
+        {
+            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
+            var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
+            string pass = Password.Text;
+            string hashedPass = HashPassword(pass);
+            IdentityResult result = manager.Create(user, hashedPass);
+            if (result.Succeeded)
+            {
+                // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                //string code = manager.GenerateEmailConfirmationToken(user.Id);
+                //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
+                //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
+
+                signInManager.SignIn( user, isPersistent: false, rememberBrowser: false);
+                Console.WriteLine("hashed: " + hashedPass);  
+                IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+            }
+            else 
+            {
+                ErrorMessage.Text = result.Errors.FirstOrDefault();
+            }
+        }
+    }
+}
